57edd65b0cff88ab3f35961e144965313ca3081e
George Hadzhiev <hadjiev.zhora@yandex.ru>
2026-03-16 12:23:45 +0300

    application.properties to application.properties.example

--- src/main/java/org/userbot/estateuserbot/EstateUserbotApplication.java
+++ src/main/java/org/userbot/estateuserbot/EstateUserbotApplication.java
@@ -3,9 +3,10 @@ package org.userbot.estateuserbot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.userbot.estateuserbot.config.TelegramProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties
+@EnableConfigurationProperties(TelegramProperties.class)
 public class EstateUserbotApplication {
 
 	public static void main(String[] args) {
